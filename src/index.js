846006b0c3066413ca170ca095ba9abcca8e1240
Jonathan Gold <jonathangold@Jonathans-Mac-mini.local>
2024-09-03 17:24:15 +0300

    Swagger added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./api/swagger');
+// const swaggerSpec = require('./swagger');
+
+const swaggerJsdoc = require('swagger-jsdoc');
+
+
+// api/utils/users.js
+
+// Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'User API',
+      version: '1.0.0',
+      description: 'API for managing users',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: [
+    './src/api/api.js',
+    './src/api/utils/users.js',
+    './src/api/utils/tasks.js'
+  
+  ],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerSpec));
 
 
 
@@ -18,13 +50,14 @@ const api = require('./api/api');
 
 
 
+
 // Simple logging middleware
 app.use((req, res, next) => {
  console.log(`${req.method} ${req.url}`);
  next(); // Passes control to the next middleware or route handler
 });
 
-// Define a route
+
 app.use('/api', api);
 
 // Start the server
