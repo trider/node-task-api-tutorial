fd2c4cff980c8ce462a18aba439fd75ddb3f332c
trider <jonnygold@gmail.com>
2024-09-06 13:11:43 +0300

    Part 1:Readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 How to build a simple task management REST API using NodeJS and the express framework. I intend to use it as a basis for a new series of articles, and serve as a resource for my current Angular and React tutorials.
 
-<!-- The tutorial includes the following installments: -->
+The tutorial includes the following installments:
 
-<!-- * [Part 1]( "react-task-tutorial-01") ReactJS for Angular Developers Part 1: Routing -->
+* [Part 1]("https://github.com/trider/node-task-api-tutorial/tree/main/node-task-api-part-01" "node-task-api-tutorial") Getting Started
--- node-task-api-part-01/index.js
+++ node-task-api-part-01/index.js
@@ -1,54 +1,12 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-const users = require('./data/users');
-const tasks = require('./data/tasks');
-
-app.use((req, res, next) => {
- console.log(`${req.method} ${req.url}`);
- next();
-});
 
 app.get('/', (req, res) => {
-  res.redirect('/api/users');
+  res.send('Welcome to the Task API');
 });
 
-app.get('/api/users', (req, res) => {
-  res.json(users);
-});
-
-app.get('/api/users/:user', (req, res) => {
-  res.json(users.find(user => user.userName === req.params.user));  
-});
-
-app.post('/api/users/login', (req, res) => {
-  if(users.find(user => user.email === req.body.email && user.password === req.body.password)){
-    res.json(
-      {
-        ...users.find(user => user.email === req.body.email && user.password === req.body.password),
-        message: 'Login successful',
-        isAuthenticated: true
-      }
-    );
-  }
-  else{
-    res.json({message: 'Login failed', isAuthenticated: false});
-  }
-
-  
-});
-
-app.get('/api/tasks/user/:user', (req, res) =>{
-  res.json(tasks.filter(task => task.user === req.params.user));
-})
-
-app.get('/api/tasks/task/:taskId', (req, res) =>{
-  res.json(tasks.find(task => task.taskId === parseInt(req.params.taskId)));
-})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-});
+})
