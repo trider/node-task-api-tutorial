133d6f675645bb931a7c71040e000fe97f454790
trider <jonnygold@gmail.com>
2024-09-08 07:16:45 +0300

    Part 1:Readme

--- node-task-api-part-01/index.js
+++ node-task-api-part-01/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 
-
 app.get('/', (req, res) => {
   res.send('Welcome to the Task API');
 });
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
