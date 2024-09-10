225a3a7e38e7fadf294d7257129e629e9fa59629
trider <jonnygold@gmail.com>
2024-09-10 11:59:02 +0300

    added part 2

--- node-task-api-part-02/index.js
+++ node-task-api-part-02/index.js
@@ -20,26 +20,26 @@ app.get('/api/users', (req, res) => {
   res.json(users);
 });
 
-// app.get('/api/users/:user', (req, res) => {
-//   res.json(users.find(user => user.userName === req.params.user));  
-// });
+app.get('/api/users/:user', (req, res) => {
+  res.json(users.find(user => user.userName === req.params.user));  
+});
 
-// app.post('/api/users/login', (req, res) => {
-//   if(users.find(user => user.email === req.body.email && user.password === req.body.password)){
-//     res.json(
-//       {
-//         ...users.find(user => user.email === req.body.email && user.password === req.body.password),
-//         message: 'Login successful',
-//         isAuthenticated: true
-//       }
-//     );
-//   }
-//   else{
-//     res.json({message: 'Login failed', isAuthenticated: false});
-//   }
+app.post('/api/users/login', (req, res) => {
+  if(users.find(user => user.email === req.body.email && user.password === req.body.password)){
+    res.json(
+      {
+        ...users.find(user => user.email === req.body.email && user.password === req.body.password),
+        message: 'Login successful',
+        isAuthenticated: true
+      }
+    );
+  }
+  else{
+    res.json({message: 'Login failed', isAuthenticated: false});
+  }
 
   
-// });
+});
 
 
 app.listen(3000, () => {
