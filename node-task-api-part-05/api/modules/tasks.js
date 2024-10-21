07bf104a9b9e682becf32e65c5d58f509081c6ee
trider <jonnygold@gmail.com>
2024-10-21 15:41:36 +0300

    Part 5: Added

---
+++ node-task-api-part-05/api/modules/tasks.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const mongoAPI = require('./mongo');
+
+router.get('/user/:user',  (req, res) =>{
+   const getData = new Promise((resolve) => {
+      mongoAPI.getItemsData(
+        { db: 'tasksDB', collection: 'tasks', query: { user: req.params.user, isActive:true } }
+      ).then((data, err) => resolve(data))
+    }).then((data) => {
+      return res.json(data)
+
+    })
+    return getData.then(data => data).catch((err) => console.log(err))
+});
+
+router.get('/task/:taskId',  (req, res) =>{
+   const getData = new Promise((resolve) => {
+      mongoAPI.getItemData(
+        { db: 'tasksDB', collection: 'tasks', query: { taskId:parseInt(req.params.taskId)} }
+      ).then((data, err) => resolve(data))
+    }).then((data) => {
+      return res.json(data)
+
+    })
+    return getData.then(data => data).catch((err) => console.log(err))
+});
+
+
+router.post('/create', (req, res) => {
+   let tasks = null
+      let taskId = null
+      const getData = new Promise((resolve) => {
+        mongoAPI.getItemsData(
+          { db: 'tasksDB', collection: 'tasks', query: {} }
+        ).then((data, err) => resolve(data))
+
+      }).then((data) => {
+        tasks = data
+        taskId = tasks.length + 1
+        return mongoAPI.writeItemData({
+          db: 'tasksDB',
+          collection: 'tasks',
+          data: {
+            ...req.body,
+            taskId: taskId,
+            added: new Date(),
+            updated: new Date(),
+            isActive: true
+
+          }
+        })
+      }).then((data) => {
+        return mongoAPI.getItemData(
+          { db: 'tasksDB', collection: 'tasks', query: { "taskId": taskId } }
+        )
+      }).then((data) => {
+          return res.json(data)
+      })
+      return getData.then(data => data).catch((err) => {
+        console.log(err)
+      })
+
+
+  
+});
+
+router.post('/update/:taskId', (req, res) => {
+   const getData = new Promise((resolve) => {
+      mongoAPI.updateItemData(
+        { db: 'tasksDB', collection: 'tasks', query: { taskId: req.params.taskId }, data: req.body }
+      ).then((data, err) => resolve(data))
+    }).then((data) => {
+
+      console.log(data.result)
+      return mongoAPI.getItemData(
+        { db: 'tasksDB', collection: 'tasks', query: { taskId: req.params.taskId } }
+      )
+    }).then((data) => {
+
+      return res.json(data)
+
+    })
+    return getData.then(data => data).catch((err) => console.log(err))
+});
+
+router.post('/delete', (req, res) => {
+  const getData = new Promise((resolve) => {
+    console.log(req.body)
+    mongoAPI.updateItemData(
+      {
+        db: 'tasksDB',
+        collection: 'tasks',
+        query: { "taskId": req.body.taskId },
+        data: {
+          isActive: false,
+          updated: new Date()
+        }
+      }
+    ).then((data, err) => resolve(data))
+  }).then((data) => {
+    console.log(data.result)
+    return mongoAPI.getItemData(
+      { db: 'tasksDB', collection: 'tasks', query: { "taskId": parseInt(req.body.taskId)} }
+    )
+  }).then((data) => {
+
+    return res.json(data)
+  })
+  return getData.then(data => data).catch((err) => {
+    console.log(err)
+  })
+});
+
+module.exports = router;
